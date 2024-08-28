ff795385fa93725d7775e3462eb7a01272bbd938
Leon Filser <leon.filser@gmail.com>
2024-08-28 16:38:31 +0200

    hope this works

--- src/main/java/Webapp.java
+++ src/main/java/Webapp.java
@@ -7,28 +7,9 @@ import java.util.Date;
 @Named
 @ApplicationScoped
 public class Webapp {
-    
-    private Collection<Ghostnet> ghostnetCollection = new ArrayList<Ghostnet>();
 
     public Webapp()
     {
-        ghostnetCollection.add(new Ghostnet(1, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(2, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(3, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(4, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(5, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(6, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(7, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(8, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(9, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-        ghostnetCollection.add(new Ghostnet(10, new Date(), new Date(), "active", 53.5511, 9.9937, 100));
-    }
-
-    public Collection<Ghostnet> getGhostnetCollection() {
-        return ghostnetCollection;
-    }
-
-    public void setGhostnetCollection(Collection<Ghostnet> ghostnetCollection) {
-        this.ghostnetCollection = ghostnetCollection;
+        
     }
 }
